acb2c381a03e2e27e880a79550be1f6e43cfba04
albertorisucci <albertorisucci@yahoo.com.br>
2019-09-02 10:08:22 -0300

    02 SpringBoot and REST Udemy

---
+++ 02 RestWithSpringBootUdemy Calculadora/src/main/java/br/com/erudio/request/converters/NumberConverter.java
@@ -0,0 +1,18 @@
+package br.com.erudio.request.converters;
+
+public class NumberConverter {
+	
+	public static Double convertToDouble(String strNumber) {
+		if (strNumber == null) return 0d;
+		String number = strNumber.replace(",", ".");		
+		if (isNumeric(number)) return Double.parseDouble(number);
+		return 0d;
+	}
+
+	public static boolean isNumeric(String strNumber) {
+		if (strNumber == null) return false;
+		String number = strNumber.replace(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+
+}
